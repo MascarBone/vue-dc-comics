5fd27c6049fab675dc67a9bb1abc82a47443b3e2
MascarBone <simone.bonelli@gmail.com>
2021-09-24 16:11:02 +0200

    Add Components, Header created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="logo" src="./assets/images/dc-logo.png">
-    <!-- '' -->
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
-// import '' from './components/''.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    // ''
+    Header,
+    Main,
+    Footer,
   }
 }
 </script>
 
 <style lang="scss">
+@import url('scss/generic.scss');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,102 @@
+<template>
+  <header>
+      <div class="row">
+          <div class="header-logo">
+              <img src="../assets/images/dc-logo.png" alt="dc-logo">
+          </div>
+          <nav class="header-nav">
+              <ul>
+                  <li v-for="(item, index) in navList" :key="index" >
+                      <a href="">{{item.link}}</a>
+                  </li>
+              </ul>
+          </nav>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data: function() {
+        return {
+            navList:[
+                {
+                   link: 'characters',
+                   url: '#',
+                },
+                {
+                   link: 'comics',
+                   url: '#',
+                },
+                {
+                   link: 'movies',
+                   url: '#',
+                },
+                {
+                   link: 'tv',
+                   url: '#',
+                },
+                {
+                   link: 'games',
+                   url: '#',
+                },
+                {
+                   link: 'collectibles',
+                   url: '#',
+                },
+                {
+                   link: 'videos',
+                   url: '#',
+                },
+                {
+                   link: 'fans',
+                   url: '#',
+                },
+                {
+                   link: 'news',
+                   url: '#',
+                },
+                {
+                   link: 'shop',
+                   url: '#',
+                },                
+            ]
+        }
+
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+    
+    .row {
+        width: 1200px;
+        height: 15vh;
+        display: flex;
+        align-items: center;
+        margin: 0 auto;
+        
+    }
+
+    .header-logo {
+        flex-grow: 1;
+    }
+
+    .header-nav {
+
+        
+        ul {
+            list-style: none;
+            display: flex;
+
+
+            a {
+                text-decoration: none;
+                margin: 0 10px;
+            }
+        }
+    }
+
+</style>>
